a34c92811a057a20080d3c486e78a219e2433bb3
Andrea Rossini <andrea@dzhendema.localdomain>
2017-03-19 16:10:34 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "network.h"
+
+using namespace std;
+
+
+int main()
+{
+	network network1;
+	vector<neuron>::iterator it = network1.neurons.insert(network1.neurons.end(), true);
+	it->
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ network.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class neuron;
+
+class synapse
+{
+private:
+	neuron& source;
+	double weight;
+
+public:
+	synapse(neuron& source, double weight);
+	double evaluate();
+};
+
+class neuron
+{
+public:
+	std::vector<synapse> inputs;
+	double output;
+
+	void evaluate();
+};
+
+class network
+{
+public:
+
+	std::vector<neuron> neurons;
+
+	void evaluate();
+};
