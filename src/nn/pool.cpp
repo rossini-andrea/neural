170785f5b8ad7c70a600ddfff1fd6b2af945009a
andrea-rossini <andrea.rossini.lc@gmail.com>
2017-03-20 23:21:46 +0100

    Creating the pool.

---
+++ src/nn/pool.cpp
@@ -0,0 +1,18 @@
+#include "pool.h"
+#include <fstream>
+
+pool::pool()
+{
+
+}
+
+pool::pool(const std::string &file)
+{
+	loadfile(file);
+}
+
+void pool::loadfile(const std::string &file)
+{
+
+
+}
