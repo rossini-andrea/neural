86b7180cf8ae5beb5470b26f1cb44767613bc7e3
Rossini Andrea <Andrea.Rossini@beta80group.it>
2017-03-22 22:32:20 +0100

    Fix bugs when loading genome file.

--- src/nn/genome.cpp
+++ src/nn/genome.cpp
@@ -11,20 +11,12 @@ void genome::generate_network()
 {
 	network.neurons.clear();
 	std::sort(genes.begin(), genes.end(), compare_genes);
+	int max = (genes.end() - 1)->destination + 1;
+	network.neurons.resize(max);
 
 	for (std::vector<gene>::iterator it = genes.begin(); it != genes.end(); ++it)
 	{
 		if (!it->enabled) continue;
-
-		int max =
-			it->destination > it->source ?
-			it->destination : it->source;
-		++max;
-
-		if (network.neurons.size() < max)
-		{
-			network.neurons.resize(max);
-		}
 
 		network.neurons[it->destination].inputs.push_back(
 			synapse(
